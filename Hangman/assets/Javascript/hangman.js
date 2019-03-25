4404c594d4c921f45feb5ec06ba00a9040410b48
montechristiaan <christiaan.simmons@live.com>
2019-03-25 11:38:23 -0500

    updated readme

--- Hangman/assets/Javascript/hangman.js
+++ Hangman/assets/Javascript/hangman.js
@@ -1,35 +1,64 @@
-var words = ["Waterloo", "SOS", "Fernando", "Mamma Mia", "Angel Eyes", "Dancing Queen", "Take a Chance on Me"];
-
+var words = ["strawberries", "raspberries", "apples", "oranges", "pineapples"];
 var randomNumber = Math.floor(Math.random() * words.length);
-var rightWord = [];
-var wrongWord = [];
+var underscores = [];
+var rightGuess = [];
+var wrongGuess =[];
 var chosenWord = words[randomNumber];
-var underScore = [];
-var blankSpaces = document.getElementsByClassName('underscore');
+var win = 0;
+var guesses = 5;
+var losses = 0;
+var num = 0;
+var gameBoard="";
+var board = document.getElementById('underscore');
 
 
-console.log(chosenWord);
-var genUnderscore = () => {
-for(var i = 0; i < chosenWord.length; i++) {
-    underScore.push("_");
-    blankSpaces[0].innerHTML = underScore.join(' ');
-    }
-    return underScore;
+for (var a=0; a<chosenWord.length; a++){
+   gameBoard=gameBoard + "<div class='letters' style='float:left' id='letter" + a + "'>_ </div>";
+
 }
-    console.log(genUnderscore());
+console.log(gameBoard);
+board.innerHTML=gameBoard  ;
 
-    document.addEventListener("keypress", (event) => {
-        var keycode = event.keyCode;
-        var keyWord = String.fromCharCode(keycode);
-        if(chosenWord.indexOf(keyWord) > -1) {
-            rightWord.push(keyWord);
-            underScore[chosenWord.indexOf(keyWord)] = keyWord;
-            blankSpaces[0].innerHTML = underScore.join(' ');
-            if(underScore.join(' ') == chosenWord) {
-                alert("You Win!");
-            }
-        }
-         
-    });
+document.onkeypress = function(event) {
+   var userGuess = event.key;
 
-    
+   console.log("My guess:" + userGuess);
+   for(var i = 0; i < chosenWord.length; i++) {
+
+       console.log("LETTER:" + chosenWord[i]);
+       var userGuess = event.key;
+
+       if (chosenWord[i]==userGuess){
+           console.log("ITS THERE! Position:" + i);
+           var current = document.getElementById('letter' + i);
+           current.innerHTML=userGuess;
+
+       }
+       else{
+           console.log("ITS NOT THERE");
+
+       }
+
+       }
+   /*
+   console.log(getunderScore())
+
+   if(userGuess === chosenWord) {
+       win++;
+       alert("You Win!")
+   }
+   else{
+       guesses--;
+       guessesLeft+++ 1;
+   }
+
+   if(guesses === 0) {
+       losses++
+       alert("You Lose!")
+   }
+   */
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # montechristiaan.github.io
 montechristiaan.github.io
+Some projects to check out...
+https://montechristiaan.github.io/GifMe/index.html
+https://montechristiaan.github.io/TriviaGame/index.html
+https://montechristiaan.github.io/TrainScheduler/index.html
