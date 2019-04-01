43706c2670a77d941a6e19c20fc23686c6a817a1
montechristiaan <christiaan.simmons@live.com>
2019-04-01 13:46:32 -0500

    still can't get spotify to work

--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -1,18 +1,26 @@
-//require("dotenv").config();
+require("dotenv").config();
 
-//var keys = require("./keys.js");
-
-//var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
 
 var axios = require("axios");
 
+var moment = require("moment");
+
+var Spotify = require("node-spotify-api");
+
+var spotify = new Spotify(keys.spotify);
+
 var nodeArgs = process.argv;
 
-var movieName ="";
+var movieName = "";
+
+var artist = "";
+
+var song = "";
 
 var command = process.argv[2];
 
-function movieThis(text) {
+function movieThis() {
 
     if(command == "movie-this") {
 
@@ -27,7 +35,7 @@ for(var i = 3; i < nodeArgs.length; i++) {
 
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-console.log(queryUrl);
+//console.log(queryUrl);
 
 axios.get(queryUrl).then(
     function(response) {
@@ -42,3 +50,50 @@ axios.get(queryUrl).then(
     }
   )}};
   movieThis();
+
+    function concertThis() {
+        
+        if(command == "concert-this") {
+            for(var i = 3; i < nodeArgs.length; i++) {
+                if(i > 3 && i < nodeArgs.length) {
+                    artist = artist + "+" + nodeArgs[i];
+                }
+                else {
+                    artist += nodeArgs[i];
+                }
+            }
+            var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+            console.log(queryUrl);
+
+            axios.get(queryUrl).then(
+                function(response) {
+                    console.log("Venue name: " + response.data[i].venue.name);
+                    console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                    console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                });
+        }
+    };
+        concertThis();
+
+        function spotifyThisSong() {
+            if(command == "spotify-this-song") {
+                for(var i = 0; i < nodeArgs.length; i++) {
+                    if(i > 3 && i < nodeArgs.length) {
+                        song = song + "+" + nodeArgs[i];
+                    }
+                    else {
+                        song += nodeArgs[i];
+                    }
+                }
+                spotify.search({type: "track", query: song}, function(err, data) {
+                    if (err) {
+                      return console.log('Error occurred: ' + err);
+                    }
+                   else {
+                  console.log(data.items); 
+                   }
+                  });
+            }
+        };
+        spotifyThisSong();
