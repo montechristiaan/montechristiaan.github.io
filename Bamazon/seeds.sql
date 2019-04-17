d08243b74b2b0b9cd17570330e3f9ce1638f2c9c
montechristiaan <christiaan.simmons@live.com>
2019-04-17 12:30:14 -0500

    added schema and seeds files

---
+++ Bamazon/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Mighty Boosh DVD", "Entertainment", 29.99, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Darkplace DVD", "Entertainment", 19.99, 32);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The IT Crowd DVD", "Entertainment", 24.99, 56);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Shazam Doll", "Toys", 11.99, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("PJ Masks HQ", "Toys", 49.99, 34);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Resident Evil 2", "Games", 59.99, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("God of War", "Games", 39.99, 13);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Great Expectations", "Books", 15.99, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Alice in Wonderland", "Books", 19.99, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hamlet", "Books", 9.99, 0);
+
+SELECT * FROM products;
+
+
+
