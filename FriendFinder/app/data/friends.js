f9dd90bd34b7e67775491257c6e2a8630441553b
montechristiaan <christiaan.simmons@live.com>
2019-04-24 15:22:19 -0500

    added permanent names to data file

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,21 +1,58 @@
 var friendArr = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Kermit the Frog",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Kermit_the_Frog.jpg/220px-Kermit_the_Frog.jpg",
         "scores":[
             5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
-      
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+
+{
+    "name":"Miss Piggy",
+    "photo":"https://i.pinimg.com/originals/64/50/57/6450577e41dd79d5b690bd309bdcf1ac.jpg",
+    "scores":[
+        1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1
+]
+},
+
+{
+    "name":"Fozzie Bear",
+    "photo":"https://vignette.wikia.nocookie.net/muppet/images/b/b5/Fozzie2.jpg/revision/latest?cb=20120410231906",
+    "scores":[
+        5,
+  5,
+  5,
+  5,
+  5,
+  1,
+  5,
+  5,
+  4,
+  5
+]
+},
+
+
+
+    
 
 ]
 
