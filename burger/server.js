0d5eb3ccaf6ef40c8499a9c6c7009e885cacd645
montechristiaan <christiaan.simmons@live.com>
2019-05-03 10:16:18 -0500

    completed server.js and main handlebars

--- burger/server.js
+++ burger/server.js
@@ -1,1 +1,25 @@
 var express = require("express");
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+var PORT = process.env.PORT || 8080;
+
+var handlebars = require("express-handlebars");
+
+app.engine("handlebars", handlebars({ defaultLayout: "main"}));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on Port: " + PORT);
+});
