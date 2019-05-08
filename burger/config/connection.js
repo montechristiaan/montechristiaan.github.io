79449b31b8798209c36bc541957dcb4922406fc4
montechristiaan <christiaan.simmons@live.com>
2019-05-08 16:17:11 -0500

    trying some more stuff

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -1,23 +1,23 @@
 var mysql = require("mysql");
 
-var connection;
-
-if(process.env.JAWSDB_URL) {
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 8889,
-        user: "root",
-        password: "root",
-        database: "burgers_db"
-        });
-    };
-
-connection.connect(function(err) {
-    if(err) throw err;
-    console.log("connected as id " + connection.threadId);
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
 });
 
+console.log(connection);
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
 module.exports = connection;
